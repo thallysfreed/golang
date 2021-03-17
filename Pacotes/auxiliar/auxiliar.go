6a3cea0eb8a7013454064d063bf736deeac81fac
Thallys Silva <thallysfreed@gmail.com>
2021-03-17 00:39:50 -0300

    Commit inicial, pacotes e módulos. public private

---
+++ Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Comentário necessário em funções exportadas
+func Escrever() {
+	fmt.Println("Escrevendo do arquivo auxiliar")
+	escrever()
+}
---
+++ Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func escrever() {
+	fmt.Println("Escrevendo do arquivo auxiliar2, método privado")
+}
